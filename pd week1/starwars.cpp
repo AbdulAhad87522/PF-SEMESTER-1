0538b28b0898da1d4d588382a277a2179050eee7
AbdulAhad87522git config --global user.name AbdulAhad87522 <ahadbahi263@gmail.com>
2024-09-15 11:14:01 +0500

    pd week1

---
+++ pd week1/starwars.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout<<"                  888888888888   8     8888        "<<endl;                                                                
+cout<<"                 88       8     8 8     8  8       "<<endl;                                                         
+cout<<"                  8888    8    88888    888        "<<endl;                                                                       
+cout<<"                     88   8    8   8    8 8        "<<endl;                                                                  
+cout<<"            8888888888    8   888 888  888 8       "<<endl;                                                                                   
+cout<<"                                                   "<<endl;                                                       
+cout<<"                                                   "<<endl;                                              
+cout<<"             8       8     8    88888   888        "<<endl;                                           
+cout<<"             8       8    8 8    8   8 8           "<<endl;                                                
+cout<<"             8   8   8   88888   8888   888        "<<endl;                               
+cout<<"              8 8 8 8    8   8   8 8       8       "<<endl;                             
+cout<<"               8   8    888 888 888 88888888       "<<endl;            
+
+}
